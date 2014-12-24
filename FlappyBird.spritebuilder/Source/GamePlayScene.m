3dcb96c70fd5c4b4ed4c33f16807a652e6a42fff
Tyler <tkui123@yahoo.com>
2014-12-23 23:26:08 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -18,6 +18,18 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+        
+    }
+    
 }
 
 // put new methods here
